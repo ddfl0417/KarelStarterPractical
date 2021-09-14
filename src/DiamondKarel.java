439ec43c41ea531cd605bc10cd58c7841cc1614d
ddfl0417 <48817585+ddfl0417@users.noreply.github.com>
2021-09-15 02:07:51 +0800

    Initial commit

---
+++ src/DiamondKarel.java
@@ -0,0 +1,21 @@
+import stanford.karel.Karel;
+
+public class DiamondKarel extends Karel {
+
+    public void run() {
+        putDiamond(5); // put beeper diamond formation with size of 5
+    }
+
+    void putDiamond(int n) {
+        /* YOUR CODE HERE */
+    }
+
+    void putRowOfBeepers(int r) {
+        /* YOUR CODE HERE */
+    }
+
+    // main method (Do not modify!)
+    public static void main(String []args){
+        (new DiamondKarel()).start(args);
+    }
+}
---
+++ worlds/11x11.w
@@ -0,0 +1,4 @@
+Dimension: (11, 11)
+Karel: (1, 1) east
+BeeperBag: INFINITE
+Speed: 1
